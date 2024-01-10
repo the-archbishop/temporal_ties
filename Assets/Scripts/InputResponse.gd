28e7c0ba094910c85018dc99bb1d7682c0955121
Bishop Bader <bflatbader@gmail.com>
2024-01-10 00:31:53 -0500

    history cleanup to save on memory

--- Assets/Scripts/InputResponse.gd
+++ Assets/Scripts/InputResponse.gd
@@ -2,5 +2,5 @@ extends VBoxContainer
 
 
 func set_text(input: String, response: String):
-	$InputHistory.text = " A > " + input
+	$InputHistory.text = " " + input
 	$Response.text = " " + response
--- Assets/Scripts/Main.gd
+++ Assets/Scripts/Main.gd
@@ -3,10 +3,20 @@ extends Control
 
 const InputResponse = preload("res://Assets/Scenes/input_response.tscn")
 
+@export var max_lines_remembered: int = 10 
+
 @onready var history_rows = $ComputerScreen/GameContainer/MarginContainer/Rows/GameInformation/Scroll/HistoryRows
 
 
 func _on_input_text_submitted(user_input):
+	if user_input.is_empty():
+		return
+	
 	var input_response = InputResponse.instantiate()
 	input_response.set_text(user_input, "This is where the response would go.")
 	history_rows.add_child(input_response)
+
+	if history_rows.get_child_count() > max_lines_remembered:
+		var rows_to_forget = history_rows.get_child_count() - max_lines_remembered
+		for i in range(rows_to_forget):
+			history_rows.get_child(i).queue_free()
