abc843ee026fbdd9712e70f828b587e15a228fb0
Bishop Bader <bflatbader@gmail.com>
2024-01-13 22:00:25 -0500

    added signals for controls and credits

--- Assets/Scripts/Title.gd
+++ Assets/Scripts/Title.gd
@@ -6,3 +6,11 @@ const INTRO = preload("res://Assets/Scenes/intro.tscn")
 func _on_start_pressed():
 	# Switch to main scene	
 	get_tree().change_scene_to_file("res://Assets/Scenes/intro.tscn")
+
+
+func _on_controls_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_pressed():
+	pass # Replace with function body.
